cc0a4f4069bfcc2feb2647bada890d41efd1e291
Ana Cáceres <ana.caceres@tii.ae>
2024-02-01 14:12:35 +0100

    FIX/fix: Remove v from version in GitHub action

---
+++ extract_release_notes.py
@@ -0,0 +1,27 @@
+def get_changelog_content():
+    with open('VERSION', 'r') as version_file:
+        version = version_file.read().split('v')[1].strip()
+        
+    with open('./docs/CHANGELOG.md', 'r') as changelog_file:
+        changelog = changelog_file.read()
+
+    version_start = changelog.find(f"## [{version}]")
+
+    if version_start == -1:
+        print(f"Version {version} not found in the changelog.")
+        return None
+
+    version_end = changelog.find("## [", version_start + 1)
+
+    if version_end == -1:
+        version_content = changelog[version_start + 1:]
+    else:
+        version_content = changelog[version_start + 1:version_end]
+
+    return version_content.strip()
+
+
+version_content = get_changelog_content()
+
+if version_content:
+    print(version_content)
