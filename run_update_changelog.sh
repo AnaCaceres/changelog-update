5742a84d2fc8fe052448b5aede77f8fabda99421
Ana Cáceres <ana.caceres@tii.ae>
2024-02-01 12:40:47 +0100

    Test release creation

--- run_update_changelog.sh
+++ run_update_changelog.sh
@@ -20,6 +20,7 @@ last_commit_message=$(git log --format=%s -n 1)
 commit_message_allowed="/develop$"
 merged_commits_from_develop_branch=$(git log --format=oneline origin/main | grep -i -E "$commit_message_allowed" | wc -l)
 pull_request_commits=()
+should_add_last_changes_to_master=false
 
 if [[ is_valid_last_commit ]]; then
 	if [ "$merged_commits_from_develop_branch" -eq 1 ]; then
@@ -29,8 +30,8 @@ if [[ is_valid_last_commit ]]; then
 	fi
 	repository_name=$1
 	if [ ${#pull_request_commits[@]} -gt 0 ]; then
-		python3 update_changelog.py --repository_url "${repository_name}" --pull_request_commits "${pull_request_commits[@]}"		
-    	if [[ $(git status --porcelain CHANGELOG.md) ]]; then
+		python3 update_changelog.py --repository_url "${repository_name}" --pull_request_commits "${pull_request_commits[@]}"
+    	if [[ $(git status --porcelain ./docs/CHANGELOG.md) ]]; then
 			should_add_last_changes_to_master=true
 		fi
 	fi
