34cce39876801dd367fb3190f2c8452de600dad7
Ana Cáceres <ana.caceres@tii.ae>
2024-02-02 13:28:19 +0100

    FIX/fix: Change release notes

--- extract_release_notes.py
+++ extract_release_notes.py
@@ -20,7 +20,10 @@ def get_changelog_content():
 
     version_content_without_header = version_content.split('\n', 1)[1]
 
+    with open('./docs/release_notes.md', 'w') as release_notes_file:
+        release_notes_file.write(version_content_without_header)
+
     return version_content_without_header
 
 
-print(get_changelog_content())
+get_changelog_content()
